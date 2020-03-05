254232db7c3ac71ecba7d95b9cbfff0593fd4f73
vasanth7781 <59653770+vasanth7781@users.noreply.github.com>
2020-03-05 07:21:44 +0530

    Update login.js

--- src/Components/login.js
+++ src/Components/login.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 // import { Route, Switch, Redirect } from 'react-router-dom';
 // import {BrowserRouter as Router,Link,Redirect} from 'react-router-dom';
 // import Route from 'react-router-dom/Route';
-import CSRFTOKEN from '../CsrfToken'
+
 
 var aesjs = require('aes-js');
 
@@ -17,49 +17,25 @@ class Login extends Component{
         username:'',
         password:'',
         isLogged:false,
-        csrf_token:'',
 
     }};
     componentDidMount (){
         if (localStorage.getItem('userToken') != null){
             this.props.history.push('/')
         }
-        this.getCookie()
     }
-    getCookie(name) {
-        if (!document.cookie) {
-          return null;
-        }
-        const token = document.cookie.split(';')
-          .map(c => c.trim())
-          .filter(c => c.startsWith(name + '='));
-    
-        if (token.length === 0) {
-          return null;
-        }
-        return decodeURIComponent(token[0].split('=')[1]);
-        
-      }
 
     changeHandler = e=>{
         this.setState({ [e.target.name]:e.target.value})
     }
     login = e =>{
-        const csrftoken = this.getCookie('csrftoken')
-        console.log(document)
-        
-        // console.log(csrftoken)
-        this.setState({
-            csrf_token: csrftoken
-        })
-        e.preventDefault()
         // console.log(e)
         fetch(('http://192.168.0.107:8000/api/users/login'),{
             method:'POST',  
             headers:{
                 'Content-Type':'application/json',
                 'Accept':'application/json',
-                'X-CSRFTOKEN': csrftoken,
+
             },
             // credentials: 'include',
             body:JSON.stringify(
@@ -110,10 +86,11 @@ class Login extends Component{
             }else{
                 this.props.history.push("/auth/Login")
             }
-            this.props.handleSuccesLogin(true)
+//             this.props.handleSuccesLogin(true)
             // window.location.href="/user/dashboard"
             // this.props.history.goBack("/user/dashboard")
         })
+        e.preventDefault()
         
     }
 
@@ -136,9 +113,7 @@ class Login extends Component{
                              <input type="checkbox" value="remember-me"/> Remember me
                             </label>
                         </div>
-                        {/* <Link to ="/user/dashboard" > */}
                         <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                        {/* </Link> */}
                         <p className="mt-5 mb-3 text-muted">© 2017-2018</p>
                    </form>
                    </div>
